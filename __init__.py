740cac1a69dd02be84aa87f88b70c5e6796be40d
Santi <72014052+santisans123@users.noreply.github.com>
2021-06-15 23:54:22 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask 
+from flask_pymongo import PyMongo
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/atorss")
+    db = mongodb_client.db
+
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/atorss"
+    mongodb_client = PyMongo(app)
+    db = mongodb_client.db
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
